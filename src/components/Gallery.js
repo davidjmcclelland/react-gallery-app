cd07856b2a5940dc502dbdf5c3db77699be95b2d
David J McClelland <davidjmcclelland@gmail.com>
2022-12-28 18:12:21 -0500

    add getURIs helper

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Image from './Image';
-
+import getURIs from '../helpers/getURIs';
 //endpoints
 // 'https://dog.ceo/api/breed/affenpinscher/images/random'
-//const breedsEndPoint = 'https://dog.ceo/api/breeds/list/all';
+const breedsEndPoint = 'https://dog.ceo/api/breeds/list/all';
 // "https://jsonplaceholder.typicode.com/users"
 
 const Gallery = () => {
@@ -11,9 +11,9 @@ const Gallery = () => {
   const [breeds, setBreeds] = useState([]);
 
   const fetchBreeds = async () => {
-
-    let breeds = ["African", "Airdale", "Beagle", "Coondog", "Doberman", "Labrador", "Pointer", "Retriever", "Setter"];;
-
+    const response = await fetch(breedsEndPoint);
+    let breeds = await response.json();
+    breeds = Object.keys(breeds.message);
     setBreeds(breeds);
   }
 
