cb6616df083596d9674b084976f29730286a8cad
David J McClelland <davidjmcclelland@gmail.com>
2022-12-28 20:49:13 -0500

    add requestRandomPics

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
-import Image from './Image';
-import getURIs from '../helpers/getURIs';
+import Image from "./Image";
+import getURIs from "../helpers/getURIs";
 //endpoints
 // 'https://dog.ceo/api/breed/affenpinscher/images/random'
-const breedsEndPoint = 'https://dog.ceo/api/breeds/list/all';
+const breedsEndPoint = "https://dog.ceo/api/breeds/list/all";
 // "https://jsonplaceholder.typicode.com/users"
 
 const Gallery = () => {
-
   const [breeds, setBreeds] = useState([]);
   const [imageRequests, setImageRequests] = useState([]);
+  const [imageList, setImageList] = useState([]);
 
   const fetchBreeds = async () => {
     const response = await fetch(breedsEndPoint);
@@ -17,21 +17,35 @@ const Gallery = () => {
     breeds = Object.keys(breeds.message);
     setBreeds(breeds);
     setImageRequests(getURIs(breeds));
-  }
+    requestRandomPics();
+  };
 
   useEffect(() => {
     fetchBreeds();
   }, []);
 
+  const requestRandomPics = async () => {
+    const links = await Promise.all(
+      imageRequests.map(async (url) => {
+        const resp = await fetch(url);
+        return resp.json();
+      })
+    );
+    const imageURLs = links.map((link) => {
+      return link.message;
+    });
+    setImageList(imageURLs);
+  };
+
   return (
     <>
       <div className="js_gallery">
         {breeds.map((breed, index) => (
-          <Image breed={breed} imageRequest={imageRequests[index]} />
+          <Image breed={breed} imageRequest={imageList[index]} />
         ))}
       </div>
     </>
   );
-}
+};
 
 export default Gallery;
