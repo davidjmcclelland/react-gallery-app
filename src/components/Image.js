2e8ad4031e3e26b38123d7f5f38c54ec577a6a71
David J McClelland <davidjmcclelland@gmail.com>
2022-12-28 21:14:49 -0500

    image src links

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,7 +3,12 @@ const Image = (props) => {
   const breed = props.breed;
   const imageRequest = props.imageRequest;
 
-  return <div className="js_img" key={breed}>{breed}<br />{imageRequest}</div>;
+  return (
+    <div className="js_img" key={breed}>
+      {breed}
+      <img src={imageRequest}></img>
+    </div>
+  );
 }
 
 export default Image;
