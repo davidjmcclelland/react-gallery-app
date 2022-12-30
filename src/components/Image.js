69727ee938282b88f38c001be6213db742e2cfa8
David J McClelland <davidjmcclelland@gmail.com>
2022-12-29 21:35:31 -0500

    useState for hover added

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,8 +1,21 @@
+import {useState} from 'react';
+
 const Image = (props) => {
+
   const breed = props.breed;
   const handleImageLoaded = props.loadedFunc;
   const index = props.index;
   const imageSrc = props.imageSrc;
+
+  const [isTooltipShown, setIsTooltipShown] = useState([false]);
+  
+  const productNameHandleHover = () => {
+    setIsTooltipShown(true);
+  };
+
+  const productNameHandleNoHover = () => {
+    setIsTooltipShown(false);
+  };
 
   return (
     <div className="js_img" key={index}>
