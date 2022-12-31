cd0cf8a84afe945b1fbdfe78f73b832a1bacc991
David J McClelland <davidjmcclelland@gmail.com>
2022-12-30 19:08:25 -0500

    added comments, improved some names, fixed a style.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -16,12 +16,18 @@ const breedsEndPoint = "https://dog.ceo/api/breeds/list/all";
     });
     return imageURLs
 };
-  
+
+// ImageLoadResults contains the gallery of images so that
+// the entire gallery visibility can be controlled once loaded.
 const ImageLoadResults = ({ images }) => {
   const [loadedImages, setLoadedImages] = useState({});
   const [ready, setReady] = useState(false);
 
+  //Image component is handled withing ImageLoadResults, so the callback function
+  // triggered by completed image loads is in this scope.
+  // src is the same url 
   function handleImageLoaded(src) {
+    
     const newLoadedImages = {
       ...loadedImages,
       [src]: true,
@@ -53,13 +59,13 @@ const ImageLoadResults = ({ images }) => {
       id="js_gallery"
       style={{ visibility: ready ? "visible" : "hidden" }}
     >
-      {images.results.map((result, index) => {
+      {images.results.map((imageURL, index) => {
         return (
           <Image
             breed={breeds[index]}
             loadedFunc={handleImageLoaded}
             index={index}
-            imageSrc={result}
+            imageSrc={imageURL}
           />
         );
       })}
--- src/components/Image.js
+++ src/components/Image.js
@@ -4,6 +4,9 @@ const Image = (props) => {
   const index = props.index;
   const imageSrc = props.imageSrc;
 
+  // handleImageLoaded is a callback function passed into Image via props
+  // when image has finished loading it triggers the callback and returns the
+  // image src url to handleImageLoaded within ImageLoadResults
   return (
     <div key={index} className="js_img">
       <div>{breed}</div>
